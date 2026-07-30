2e4db4b721dabb811f2db9696122825dbed8be77
EsmaaelX <esmaael.esmaael@gmail.com>
2026-07-30 21:17:52 +0300

    Update mail_config.php

--- Rently/includes/mail_config.php
+++ Rently/includes/mail_config.php
@@ -4,8 +4,8 @@
 // SMTP Configuration
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 587);
-define('SMTP_USER', 'esmaael.esmaael@gmail.com');
-define('SMTP_PASS', 'arir ltge dcct zctj');
+define('SMTP_USER', '');
+define('SMTP_PASS', '');
 define('SMTP_SECURE', 'tls');
 
 // Sender Information
